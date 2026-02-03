652b9feaba47c140c91f1ab4bd6adb083b5735fd
uitesterhero <150478429+uitesterhero@users.noreply.github.com>
2026-02-03 14:05:43 -0600

    Comment out tests in ThirdSuiteTest.java
    Comment out tests and sections for a pull request that will not be merged.

--- src/test/java/basic/unittests/ThirdSuiteTest.java
+++ src/test/java/basic/unittests/ThirdSuiteTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ThirdSuiteTest {
-  /* Comment out from here to line 23 for the pr you will not merge   */
+  /* Comment out from here to line 23 for the pr you will not merge    
     
   @Test
     public void thirdTest1() {
@@ -21,8 +21,8 @@ public class ThirdSuiteTest {
     public void thirdTest3() {
         System.out.println("thirdTest3 successful!");
     } 
-   
-    /* Comment out from here to the end for the original file  
+   */
+    /* Comment out from here to the end for the original file  */
     @Disabled
     @Test
     public void skippedTest1() {
@@ -39,5 +39,5 @@ public class ThirdSuiteTest {
     public void errorTest3() throws Exception {
         throw new Exception("Throwing an error");
     }
-   */ 
+   
 }
